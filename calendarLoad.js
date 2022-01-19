650455a4ec12a33634bdc389a2eb5c5a999818a0
xc46 <52637873+xc46@users.noreply.github.com>
2022-01-18 23:40:31 -0500

    Highlighted current day

--- calendarLoad.js
+++ calendarLoad.js
@@ -1,9 +1,18 @@
 let current_date = new Date();
 let calendar_date = new Date(current_date.getFullYear(), current_date.getMonth(), 1);
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const rows = ["0", "1", "2", "3", "4", "5"];
 
-console.log(calendar_date)
+function getDayCoord(i) {
+	const real_i = i + calendar_date.getDay() - 1; // + 1 for 0 -> 1
+	const x = real_i % 7;
+	const y = Math.floor(real_i / 7);
+
+	return [x, y];
+}
+
+function getID(x, y) {
+	const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	return days[x] + String(y);
+}
 
 function loadCalendar() {
 	let i = 0;
@@ -18,12 +27,20 @@ function loadCalendar() {
 			label = String(real_i);
 		}
 
-		const id = days[x] + String(y);
-		console.log(real_i);
-		console.log(id);
-		console.log(label);
-		console.log("========");
-		document.getElementById(id).innerHTML = label;
+		document.getElementById(getID(x, y)).innerHTML = label;
 	}
 }
+
+function highlightDate(date) {
+	if (date.getMonth() != calendar_date.getMonth()) {
+		return;
+	}
+
+	const coord = getDayCoord(date.getDate());
+	const x = coord[0];
+	const y = coord[1];
+	document.getElementById(getID(x, y)).style.backgroundColor = "yellow";
+}
+
 loadCalendar();
+highlightDate(current_date);
